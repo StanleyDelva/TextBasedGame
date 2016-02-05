e286a4efc4cb771dd3685607a30abefcfd3ea724
Strikaa <mastergexpert@gmail.com>
2016-02-05 18:40:23 -0500

    Made new functions
    made new functions for the main menu and the "how to play" option.
    Having trouble starting the "play" option so I'll get into that later.

--- TxtBasedGame/main.cpp
+++ TxtBasedGame/main.cpp
@@ -3,20 +3,37 @@
 
 using namespace std;
 
+int MainMenu()
+{
+  cout << "            \n                                   Get a Life...                                                   " << endl; // Title
+  cout << "     MAIN MENU      \n [1] How to Play  \n [2] Play \n "; // Main Menu I'm thinking of making a function for the main menu.
+  return 0;
+}
+
+int HowtoPlay()
+{
+    cout << " Press the number in the Brackets[] to choose an option on the main menu or in-game. " << endl; // How To Play
+    cout << "                   Do You want to go back to the Main Menu? \n [1] Go back to Main Menu              "; // Asks users whether he wants to go back or not.
+}
+
 int main()
 {
-    int UserInput = 0;//User's input
-    cout << "            \n                                   Get a Life...                                                   " << endl; // Title
-    cout << "     MAIN MENU      \n [1] How to Play  \n [2] Play \n "; // Main Menu I'm thinking of making a function for the main menu.
-    cin >> UserInput; // Takes in user's input
+    MainMenu();
+    int UserInput = 0;
+    cin >> UserInput; // Takes in user input
     if (UserInput = 1) // If user chooses "How to Play"
     {
-        cout << " Press the number in the Brackets[] to choose an option on the main menu or in-game. " << endl; // How To Play
-        cout << "                   Do You want to go back to the Main Menu? \n [1] Yes \n [2] No              "; // Asks users whether he wants to go back or not.
+       HowtoPlay();
         cin >> UserInput;
-        if (UserInput = 1) // If user chooses option 1, I want to send them back to the main menu...
+        if (UserInput = 1)  // If user chooses option 1 (How to play)
+        {
+            MainMenu();
+        }
     }
-
+    else if(UserInput = 2) // If user chooses option 2 (Play)
+        {
+            cout << "This game is a game of choices..." << endl;
+        }
 
 
     return 0;
